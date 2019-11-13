bc31fab05d9cfeb32ddb4122031d419c63770674
Xuyang Kang <xuyangkang@gmail.com>
2019-11-14 01:28:45 +0900

    Refactor

---
+++ convert_train_data.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import utils
+import click
+import tensorflow.keras as keras
+
+
+@click.command()
+@click.option('--train', default='./train.csv', help='The given train file')
+@click.option('--vecs', default='./word_vec.npy', help='The word vector mmap file')
+@click.option('--words', default='./words.txt', help='The word list')
+def convert_train_data(train, vecs, words):
+    row = 916750
+    col = 300
+    util = utils.Utils(vecs, row, col, words)
+    df = pd.read_csv(train)
+    data_x = []
+    data_y = []
+
+    for index, row in df.iterrows():
+        x = util.embed_sentense(row['text'])
+        x = np.append(x, [int(row['sex']) - 1, float(row['age']) / 100])
+        y = np.array([int(row['event'])])
+        data_x.append(x)
+        data_y.append(y)
+
+    data_x = np.array(data_x)
+    data_y = np.array(data_y)
+    data_y = keras.utils.to_categorical(data_y, 100)
+
+    np.save('data_x.npy', data_x)
+    np.save('data_y.npy', data_y)
+
+if __name__ == '__main__':
+    convert_train_data()
--- train.py
+++ train.py
@@ -12,53 +12,18 @@ from tensorflow.keras.models import Sequential
 @click.option('--vecs', default='./word_vec.npy', help='The word vector mmap file')
 @click.option('--words', default='./words.txt', help='The word list')
 def train_model(train, vecs, words):
-    row = 916750
-    col = 300
-    util = utils.Utils(vecs, row, col, words)
-    df = pd.read_csv(train)
-    train_size = df.shape[0] // 10 * 9
-    test_size = df.shape[0] - train_size
-    train_x = []
-    train_y = []
-    test_x = []
-    test_y = []
-
-    for index, row in df.iterrows():
-        x = util.embed_sentense(row['text'])
-        x = np.append(x, [int(row['sex']) - 1, float(row['age']) / 100])
-        y = np.array([int(row['event'])])
-        if index < train_size:
-            train_x.append(x)
-            train_y.append(y)
-        else:
-            test_x.append(x)
-            test_y.append(y)
-
-    train_x = np.array(train_x)
-    train_y = np.array(train_y)
-    test_x = np.array(test_x)
-    test_y = np.array(test_y)
-
-    train_y = keras.utils.to_categorical(train_y, 100)
-    test_y = keras.utils.to_categorical(test_y, 100)
-
-    print(train_x.shape)
-    print(train_y.shape)
-    print(test_x.shape)
-    print(test_y.shape)
+    data_x = np.load('data_x.npy')
+    data_y = np.load('data_y.npy')
 
     model = Sequential()
-    model.add(Dense(units=400, activation='relu', input_shape=(302,)))
-    model.add(Dense(units=400, activation='relu'))
+    model.add(Dense(units=200, activation='relu', input_shape=(302,)))
+    model.add(Dense(units=200, activation='relu'))
     model.add(Dense(units=100, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
                   optimizer='sgd',
                   metrics=['accuracy'])
 
-    model.fit(train_x, train_y, epochs=30, batch_size=20)
-    score = model.evaluate(test_x, test_y)
-
-    print(score)
+    model.fit(data_x, data_y, epochs=30, batch_size=20, validation_split=0.1)
 
 
 if __name__ == '__main__':
